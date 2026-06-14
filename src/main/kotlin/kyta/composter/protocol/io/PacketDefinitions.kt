6c92f55b936e97f97d45bd9d77a7af491cedf886
Apollointhehouse <me@apollointhehouse.dev>
2026-06-14 22:50:53 +1200

    Updated readme

--- src/main/kotlin/kyta/composter/protocol/io/PacketDefinitions.kt
+++ src/main/kotlin/kyta/composter/protocol/io/PacketDefinitions.kt
@@ -7,13 +7,13 @@ import kotlin.reflect.KClass
 internal class PacketDefinitions(
     private val state: ConnectionState,
     internal val packetIds: MutableMap<KClass<out Packet>, Int> = mutableMapOf(),
-    internal val serializers: Array<PacketSerializer<out Packet>?> = arrayOfNulls(256),
+    internal val serializers: Array<PacketSerializer<Packet>?> = arrayOfNulls(256),
 ) {
     fun getId(type: KClass<out Packet>): Int {
         return packetIds[type] ?: error("${type.simpleName} does not have an assigned id for state ${state.name}.")
     }
 
-    fun getSerializer(id: Int) : PacketSerializer<out Packet>? {
+    fun getSerializer(id: Int) : PacketSerializer<Packet>? {
         return serializers[id]
     }
 }
